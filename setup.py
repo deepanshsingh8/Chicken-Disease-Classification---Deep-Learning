8db788f01f1d82e22d70c73e5a2340e9927244de
deepanshsingh8 <50017951+deepanshsingh8@users.noreply.github.com>
2023-11-14 02:00:06 +0530

    Added the workflow and logging into __init__ files

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import setuptools
 
-with open("README.md", "r", encoding="utf-g") as f:
+with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
 __version__ = "0.0.0"
